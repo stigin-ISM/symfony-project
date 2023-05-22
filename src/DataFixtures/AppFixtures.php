31bce00daca2df15bfa5740f3218ecb6f2275ef2
stigin <stiginstarview@gmail.com>
2023-05-22 16:00:03 +0200

    dataFixtures are in place

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
--- src/DataFixtures/carFixtures.php
+++ src/DataFixtures/carFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Cars;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,6 +11,27 @@ class carFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $car = new Cars();
+        $car->setMake('BMW');
+        $car->setModel('M8');
+        $car->setYear(2022);
+        $car->setColor("Light Blue");
+        $car->setSeller("Iven");
+        $car->setDescription("Rarely driven- used by the elderly ");
+        $car->setImagePath("https://www.bmw.de/content/dam/bmw/common/all-models/m-series/m8-coupe/2022/onepager/bmw-m8-coupe-onepager-sp-desktop.jpg/jcr:content/renditions/cq5dam.resized.img.1680.large.time1642518221018.jpg");
+        $manager->persist($car);
+
+        $car2 = new Cars();
+        $car2->setMake('VW');
+        $car2->setModel('Golf');
+        $car2->setYear(2010);
+        $car2->setColor("Pink");
+        $car2->setSeller("Richard");
+        $car2->setDescription("Mr.Big wagon");
+        $car2->setImagePath("https://www.autopfister.ch/wp-content/uploads/sites/8/2018/01/pink_golf.jpg");
+        $manager->persist($car2);
+
+        $manager->flush();
+
 
     }
 }
