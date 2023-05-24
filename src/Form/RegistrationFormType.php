74717f2c51f70bb6c085d894fba2103e227b3c62
stigin <stiginstarview@gmail.com>
2023-05-24 23:50:07 +0200

    Beta version

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,10 +6,12 @@ use App\Entity\Admin;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -19,24 +21,23 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', TextType::class, [
-                'label' => false,
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
                 'attr' => [
                     'autocomplete' => 'email',
                     'class' => 'bg-transparent block mt-10 mx-auto border-b-2 w-1/5 h-20 text-2xl outline-none',
                     'placeholder' => 'Email'
                 ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your Email',
+                    ]),
+                    ]
 
             ])
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+
             ->add('plainPassword', PasswordType::class, [
+
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'label' => false,
@@ -58,6 +59,14 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
         ;
     }
 
@@ -68,3 +77,5 @@ class RegistrationFormType extends AbstractType
         ]);
     }
 }
+
+
